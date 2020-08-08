924e905c4ae11ccee7513657afaaf619262c518b
izerozlu <izerozlu@gmail.com>
2020-08-08 22:20:29 +0300

    :construction: Detail view improved.

--- src/index.js
+++ src/index.js
@@ -13,19 +13,23 @@ const MOCKS = {
       id: '67abb5e7-c471-46d9-9f9a-574eff8b2878',
       title: 'International Pandemic Discussion MeetUp',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</br>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        '<h3>Pretium Aenean Pharetra</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at risus viverra adipiscing at in tellus integer. Mauris in aliquam sem fringilla. Tristique senectus et netus et malesuada fames. Metus aliquam eleifend mi in nulla posuere sollicitudin. Odio eu feugiat pretium nibh. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Gravida cum sociis natoque penatibus et magnis dis parturient. Purus ut faucibus pulvinar elementum integer enim neque. Purus faucibus ornare suspendisse sed nisi lacus sed. Pellentesque dignissim enim sit amet venenatis urna cursus. Vitae ultricies leo integer malesuada nunc. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi.</br></br><h3>Volutpat Maecenas</h3>Eu non diam phasellus vestibulum lorem sed. Viverra aliquet eget sit amet tellus. Phasellus faucibus scelerisque eleifend donec pretium vulputate. Purus in mollis nunc sed id. Odio ut enim blandit volutpat maecenas volutpat blandit. Nunc mattis enim ut tellus elementum sagittis. Nibh ipsum consequat nisl vel pretium lectus quam id. Consequat mauris nunc congue nisi vitae suscipit. Elementum integer enim neque volutpat ac tincidunt vitae. Turpis in eu mi bibendum neque. Ut morbi tincidunt augue interdum velit euismod in pellentesque.</br></br>At varius vel pharetra vel turpis. Mi in nulla posuere sollicitudin. Sed vulputate odio ut enim blandit volutpat maecenas. Risus sed vulputate odio ut enim blandit. Senectus et netus et malesuada fames ac turpis. Arcu non sodales neque sodales ut etiam sit amet nisl. Dis parturient montes nascetur ridiculus mus mauris. Elit duis tristique sollicitudin nibh sit amet commodo nulla. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Magna fringilla urna porttitor rhoncus dolor purus non enim. Condimentum id venenatis a condimentum vitae. Sagittis id consectetur purus ut. Eget egestas purus viverra accumsan in nisl. In nisl nisi scelerisque eu.',
       date: '2020-08-18T18:00:00',
       type: EVENT_TYPES.MEETUP,
       location: 'Online',
+      locationLink: 'https://www.youtube.com/watch?v=IXLnoqQV5wE',
+      locationLinkText: 'Meetup Community',
     },
     {
       id: 'a2a7fb57-1ad1-4bdf-83e6-2e79f97f0701',
       title: '2020 Annual VanHackathon',
       description:
-        'Etiam erat velit scelerisque in dictum. Amet luctus venenatis lectus magna fringilla urna porttitor. Proin libero nunc consequat interdum varius sit amet mattis. Lacus sed turpis tincidunt id aliquet. Habitant morbi tristique senectus et netus. Euismod quis viverra nibh cras pulvinar mattis nunc sed.',
+        '<h3>Pretium Aenean Pharetra</h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tortor at risus viverra adipiscing at in tellus integer. Mauris in aliquam sem fringilla. Tristique senectus et netus et malesuada fames. Metus aliquam eleifend mi in nulla posuere sollicitudin. Odio eu feugiat pretium nibh. Phasellus vestibulum lorem sed risus ultricies tristique nulla aliquet enim. Gravida cum sociis natoque penatibus et magnis dis parturient. Purus ut faucibus pulvinar elementum integer enim neque. Purus faucibus ornare suspendisse sed nisi lacus sed. Pellentesque dignissim enim sit amet venenatis urna cursus. Vitae ultricies leo integer malesuada nunc. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra. Aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi.',
       date: '2020-02-11T18:00:00',
       type: EVENT_TYPES.VANHACKATHON,
       location: 'Online',
+      locationLink: 'https://www.youtube.com/watch?v=IXLnoqQV5wE',
+      locationLinkText: 'Register here',
       landscapeImage:
         'https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1490&q=80',
     },
@@ -33,10 +37,12 @@ const MOCKS = {
       id: 'a3b5ed70-1624-471e-bf69-91337d0bf79b',
       title: 'Toronto Developer Conference 2020',
       description:
-        'At auctor urna nunc id cursus metus aliquam eleifend mi. Blandit cursus risus at ultrices mi tempus imperdiet nulla malesuada.</br>Ac placerat vestibulum lectus mauris ultrices eros. Gravida dictum fusce ut placerat orci. Lobortis feugiat vivamus at augue eget arcu dictum varius duis.</br>Consectetur a erat nam at lectus.',
+        '<h3>Volutpat Maecenas</h3>Eu non diam phasellus vestibulum lorem sed. Viverra aliquet eget sit amet tellus. Phasellus faucibus scelerisque eleifend donec pretium vulputate. Purus in mollis nunc sed id. Odio ut enim blandit volutpat maecenas volutpat blandit. Nunc mattis enim ut tellus elementum sagittis. Nibh ipsum consequat nisl vel pretium lectus quam id. Consequat mauris nunc congue nisi vitae suscipit. Elementum integer enim neque volutpat ac tincidunt vitae. Turpis in eu mi bibendum neque. Ut morbi tincidunt augue interdum velit euismod in pellentesque.</br></br>At varius vel pharetra vel turpis. Mi in nulla posuere sollicitudin. Sed vulputate odio ut enim blandit volutpat maecenas. Risus sed vulputate odio ut enim blandit. Senectus et netus et malesuada fames ac turpis. Arcu non sodales neque sodales ut etiam sit amet nisl. Dis parturient montes nascetur ridiculus mus mauris. Elit duis tristique sollicitudin nibh sit amet commodo nulla. Pretium aenean pharetra magna ac placerat vestibulum lectus mauris. Magna fringilla urna porttitor rhoncus dolor purus non enim. Condimentum id venenatis a condimentum vitae. Sagittis id consectetur purus ut. Eget egestas purus viverra accumsan in nisl. In nisl nisi scelerisque eu.',
       date: '2020-08-24T19:30:00',
       type: EVENT_TYPES.LEAP,
       location: 'Online',
+      locationLink: 'https://www.youtube.com/watch?v=IXLnoqQV5wE',
+      locationLinkText: 'Event program',
     },
     {
       id: 'bcfb97e0-1f00-4c60-b0e2-9f9091ec2d70',
@@ -46,6 +52,9 @@ const MOCKS = {
       date: '2020-11-06T17:30:00',
       type: EVENT_TYPES.PREMIUM_WEBINAR,
       location: 'Ontario, Canada',
+      locationLink: 'https://goo.gl/maps/KqbqrcFpSw2ctrZM7',
+      locationLinkText: 'View on map',
+      video: '//player.vimeo.com/video/399003581?title=0&amp;portrait=0&amp;byline=0&amp;autoplay=1;loop=1;controls=0',
       heroImage:
         'https://images.unsplash.com/photo-1555421689-43cad7100750?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
     },
@@ -57,6 +66,8 @@ const MOCKS = {
       date: '2020-03-18T18:00:00',
       type: EVENT_TYPES.RECRUITING_MISSION,
       location: 'Online',
+      locationLink: 'https://www.youtube.com/watch?v=IXLnoqQV5wE',
+      locationLinkText: 'Video',
       heroImage:
         'https://images.unsplash.com/photo-1544723795-3fb6469f5b39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=635&q=80',
     },
@@ -68,6 +79,8 @@ const MOCKS = {
       date: '2020-07-21T17:00:00',
       type: EVENT_TYPES.OPEN_WEBINAR,
       location: 'Online',
+      locationLink: 'https://www.youtube.com/watch?v=IXLnoqQV5wE',
+      locationLinkText: 'Video',
       landscapeImage:
         'https://images.unsplash.com/photo-1556761175-129418cb2dfe?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80',
     },
@@ -188,7 +201,22 @@ class DateHelper {
     const month = months[this.date.getMonth()];
     const year = this.date.getFullYear();
 
-    return `${month}${delimiter}${day},${delimiter}${year}`;
+    return `${month}${delimiter}${this.normalizeNumber(day)},${delimiter}${year}`;
+  };
+
+  toTime = (delimiter = ':') => {
+    const hours = this.date.getHours();
+    const minutes = this.date.getMinutes();
+
+    return `${this.normalizeNumber(hours)}${delimiter}${this.normalizeNumber(minutes)}`;
+  };
+
+  normalizeNumber = (number) => {
+    if (number < 10) {
+      return '0' + number;
+    } else {
+      return number;
+    }
   };
 }
 
@@ -208,15 +236,30 @@ class EventTypeHelper {
 // Models
 
 class Event {
-  constructor({ title, description, date, type, location, id, heroImage, landscapeImage }) {
+  constructor({
+    title,
+    description,
+    date,
+    type,
+    location,
+    locationLink,
+    locationLinkText,
+    id,
+    heroImage,
+    landscapeImage,
+    video,
+  }) {
     this.title = title;
     this.description = description;
     this.date = new Date(date);
     this.type = type;
     this.location = location;
+    this.locationLink = locationLink;
+    this.locationLinkText = locationLinkText;
     this.id = id;
     this.heroImage = heroImage;
     this.landscapeImage = landscapeImage;
+    this.video = video;
 
     this.eventPhotos = [];
     this.eventCard = null;
@@ -226,10 +269,9 @@ class Event {
 
   configureEventPhotos = async () => {
     new Array(Random.positiveInteger(10, 4)).fill(0).forEach(async (item, index) => {
-      const height = Random.positiveInteger(400, 200);
-      const width = Random.positiveInteger(400, 200);
+      const seed = Random.positiveInteger(1000, -1000);
 
-      const imageUrl = `https://picsum.photos/${height}/${width}?random=${index}`;
+      const imageUrl = `https://picsum.photos/320/180?random=${seed + index}`;
       this.eventPhotos.push(imageUrl);
     });
   };
@@ -242,10 +284,14 @@ class Event {
 
     this.setCardType();
     this.setCardHeader();
+    this.setCardDescription();
     this.setCardDate();
     this.setCardLocation();
+    this.setCardEventPhotos();
     this.setCardHeroImage();
     this.setCardLandscapeImage();
+    this.setCardVideo();
+
     this.addClickListener();
 
     return this.eventCard;
@@ -258,12 +304,15 @@ class Event {
         break;
       case EVENT_TYPES.LEAP:
         this.eventCard.classList.add('event-card--leap');
+        this.eventCard.classList.add('event-card--distinctive');
         break;
       case EVENT_TYPES.RECRUITING_MISSION:
         this.eventCard.classList.add('event-card--recruiting-mission');
+        this.eventCard.classList.add('event-card--distinctive');
         break;
       case EVENT_TYPES.VANHACKATHON:
         this.eventCard.classList.add('event-card--vanhackathon');
+        this.eventCard.classList.add('event-card--distinctive');
         break;
       case EVENT_TYPES.PREMIUM_WEBINAR:
         this.eventCard.classList.add('event-card--premium-webinar');
@@ -290,14 +339,22 @@ class Event {
     cardSubtitle.textContent = new EventTypeHelper(this.type).toText();
   };
 
+  setCardDescription = () => {
+    const cardDescription = this.eventCard.querySelector('.event-card__description');
+    cardDescription.innerHTML = this.description;
+  };
+
   setCardDate = () => {
     const cardDate = this.eventCard.querySelector('.event-card__date');
+    const cardDateTime = this.eventCard.querySelector('.event-card__date-time');
     cardDate.textContent = new DateHelper(this.date).toHumanReadable();
+    cardDateTime.textContent = new DateHelper(this.date).toTime();
   };
 
   setCardLocation = () => {
     const cardLocationContainer = this.eventCard.querySelector('.event-card__location-container');
     const cardLocation = cardLocationContainer.querySelector('.event-card__location');
+    const cardLocationLink = cardLocationContainer.querySelector('.event-card__location-link');
     cardLocation.textContent = this.location;
 
     if (this.location.toLowerCase() === 'online') {
@@ -305,6 +362,24 @@ class Event {
     } else {
       cardLocationContainer.classList.add('event-card__location--physical');
     }
+
+    cardLocationLink.setAttribute('href', this.locationLink);
+    cardLocationLink.textContent = this.locationLinkText;
+  };
+
+  setCardEventPhotos = () => {
+    const cardImages = this.eventCard.querySelector('.event-photos__image-container');
+
+    this.eventPhotos.forEach((eventPhoto) => {
+      const imageElement = document.createElement('img');
+      imageElement.setAttribute('src', eventPhoto);
+      imageElement.setAttribute('alt', `Event photo for the ${this.title} event.`);
+      imageElement.setAttribute('height', '180');
+      imageElement.setAttribute('width', '320');
+      imageElement.classList.add('event-photos__image');
+
+      cardImages.append(imageElement);
+    });
   };
 
   setCardHeroImage = () => {
@@ -327,9 +402,32 @@ class Event {
     }
   };
 
+  setCardVideo = () => {
+    const iframeWrapper = this.eventCard.querySelector('.event-card__iframe-wrapper');
+    if (this.video) {
+      const iframe = document.createElement('iframe');
+      iframe.setAttribute('src', this.video);
+      iframe.setAttribute('frameborder', '0');
+      iframe.setAttribute('loading', 'lazy');
+
+      iframeWrapper.append(iframe);
+    } else {
+      iframeWrapper.remove();
+    }
+  };
+
   addClickListener = () => {
     this.eventCard.addEventListener('click', (event) => {
-      console.log(this);
+      const toggledEventCard = document.querySelector('.event-card--toggled');
+      if (toggledEventCard) {
+        toggledEventCard.classList.remove('event-card--toggled');
+      }
+
+      this.eventCard.classList.add('event-card--toggled');
+
+      setTimeout(() => {
+        this.eventCard.scrollIntoView({ behavior: 'smooth' });
+      }, 120);
     });
   };
 }
